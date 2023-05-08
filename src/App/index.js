75d3ea6158382749ae48b561b9f93268c0a76ad0
Aleksndra Świtacz <switaczaleksandra@gmail.com>
2023-05-08 14:00:26 +0200

    date added

--- src/App/index.js
+++ src/App/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import { Form } from "./Form";
 import { currencies } from "./currencies";
+import { Clock } from "./Clock";
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
 
     return (
         <div className="app">
+            <Clock />
             <Form 
             result={result}
             calculateResult={calculateResult}
